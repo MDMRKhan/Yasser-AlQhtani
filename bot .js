e4d9eefa7169fd5d9c9144d7d0919de28089454b
MDMRKhan <46194868+MDMRKhan@users.noreply.github.com>
2018-12-27 17:17:36 +0300

    Update and rename bot (1).js to bot .js

---
+++ bot .js
@@ -0,0 +1,41 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+var prefix = "y";
+client.on('message', n3k4a => {
+if(n3k4a.content.startsWith(prefix + 'email')) {
+function randomem() {
+let email = '';
+const ReBeL = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789._"\'';
+for (let i = 0; i < 5; i++) email += ReBeL.charAt(Math.floor(Math.random() * ReBeL.length));
+return email;
+}
+function randompass() {
+let pass = '';
+const CoDeS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789.!?@#$%&()-_"\'';
+for (let i = 0; i < 8; i++) pass += CoDeS.charAt(Math.floor(Math.random() * CoDeS.length));
+return pass;
+}
+const random1 = randomem();
+const random2 = randompass();
+n3k4a.channel.send(`email : **${random1}@gmail.com**
+password : **${random2}**`);
+}});
+
+
+
+
+
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Cyhper Script By : DREAM`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : DREAM ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`yplay|By Yasser AlQahtani##6021`,"http://twitch.tv/YouTube")
+client.user.setStatus("dnd")
+});
+
+
+
+client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
