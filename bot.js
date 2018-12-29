96c94583ebc589aba0e5609cf654e6a822cf0160
MDMRKhan <46194868+MDMRKhan@users.noreply.github.com>
2018-12-29 19:07:19 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -371,3 +371,8 @@ if(!g.me.voiceChannel) c.join();
 console.log("Failed To Join:\n The Channel Type isn't \"text\"");
 }//Toxic Codes
 });
+
+client.login(process.env.BOT_TOKEN);
+
+client.on("ready", async  => {
+  setInterval(function(){
